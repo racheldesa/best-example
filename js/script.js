5a77d00286bbaeee5f4b611f2e8a93dd42c10c4a
racheldesa <racheldesa@outlook.com>
2022-10-12 13:44:32 -0400

    Add text to the README

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener(
     "DOMContentLoaded",
     function(event){
-        alert('The page is loaded and the script ran!')
+        var message = document.getElementById('message')
+        message.innerText = 'The script ran!'
     }
 );
